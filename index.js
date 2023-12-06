3099845cabf88b8769dc6f501a928918a199544f
GuilhermeMMenezes <137406003+GuilhermeMMenezes@users.noreply.github.com>
2023-12-06 07:39:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,188 @@
+let modal = document.getElementById("modal");
+let botao = document.getElementById("botao");
+let fechar = document.getElementById("fechar");
+
+botao.onclick = function (){
+    modal.style.display = "block";
+}
+
+fechar.onclick = function(){
+    modal.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal.style.display = "none";
+    }
+}
+
+
+let modal2 = document.getElementById("modal2");
+let botao2 = document.getElementById("botao2");
+let fechar2 = document.getElementById("fechar2");
+
+botao2.onclick = function (){
+    modal2.style.display = "block";
+}
+
+fechar2.onclick = function(){
+    modal2.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal2.style.display = "none";
+    }
+}
+
+
+let modal3 = document.getElementById("modal3");
+let botao3 = document.getElementById("botao3");
+let fechar3 = document.getElementById("fechar3");
+
+botao3.onclick = function (){
+    modal3.style.display = "block";
+}
+
+fechar3.onclick = function(){
+    modal3.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal3.style.display = "none";
+    }
+}
+
+
+let modal4 = document.getElementById("modal4");
+let botao4 = document.getElementById("botao4");
+let fechar4 = document.getElementById("fechar4");
+
+botao4.onclick = function (){
+    modal4.style.display = "block";
+}
+
+fechar4.onclick = function(){
+    modal4.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal4.style.display = "none";
+    }
+}
+
+
+let modal5 = document.getElementById("modal5");
+let botao5 = document.getElementById("botao5");
+let fechar5 = document.getElementById("fechar5");
+
+botao5.onclick = function (){
+    modal5.style.display = "block";
+}
+
+fechar5.onclick = function(){
+    modal5.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal5.style.display = "none";
+    }
+}
+
+
+let modal6 = document.getElementById("modal6");
+let botao6 = document.getElementById("botao6");
+let fechar6 = document.getElementById("fechar6");
+
+botao6.onclick = function (){
+    modal6.style.display = "block";
+}
+
+fechar6.onclick = function(){
+    modal6.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal6.style.display = "none";
+    }
+}
+
+
+let modal7 = document.getElementById("modal7");
+let botao7 = document.getElementById("botao7");
+let fechar7 = document.getElementById("fechar7");
+
+botao7.onclick = function (){
+    modal7.style.display = "block";
+}
+
+fechar7.onclick = function(){
+    modal7.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal7.style.display = "none";
+    }
+}
+
+
+let modal8= document.getElementById("modal8");
+let botao8 = document.getElementById("botao8");
+let fechar8 = document.getElementById("fechar8");
+
+botao8.onclick = function (){
+    modal8.style.display = "block";
+}
+
+fechar8.onclick = function(){
+    modal8.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal8.style.display = "none";
+    }
+}
+
+
+let modal9 = document.getElementById("modal9");
+let botao9 = document.getElementById("botao9");
+let fechar9 = document.getElementById("fechar9");
+
+botao9.onclick = function (){
+    modal9.style.display = "block";
+}
+
+fechar9.onclick = function(){
+    modal9.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal9.style.display = "none";
+    }
+}
+
+
+let modal10 = document.getElementById("modal10");
+let botao10 = document.getElementById("botao10");
+let fechar10 = document.getElementById("fechar10");
+
+botao10.onclick = function (){
+    modal10.style.display = "block";
+}
+
+fechar10.onclick = function(){
+    modal10.style.display = "none";
+}
+
+window.onclick = function(Event){
+    if(Event.target == modal){
+        modal10.style.display = "none";
+    }
+}
